5e69df0fe3c7376e4c049742b27709bb324524b0
Roland Tanglao <roland@rolandtanglao.com>
2023-05-13 23:20:40 -0700

    add get by creator

---
+++ get-answers-by-creator.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'bundler/setup'
+require 'typhoeus'
+require 'amazing_print'
+require 'json'
+require 'time'
+require 'date'
+require 'csv'
+require 'logger'
+require 'pry'
+require_relative 'get-kitsune-response'
+require_relative 'fix-kludged-time'
+
+logger = Logger.new(STDERR)
+logger.level = Logger::DEBUG
+
+if ARGV.empty?
+  puts "usage: #{$PROGRAM_NAME} <creator>"
+  exit
+end
+CREATOR = ARGV[0]
+logger.debug "CREATOR: #{CREATOR}"
+url_params = {
+  format: 'json',
+  creator: CREATOR
+}
+csv = []
+
+url = 'https://support.mozilla.org/api/2/answer/'
+end_program = false
+answer_number = 0
+csv = []
+until end_program
+  answers = getKitsuneResponse(url, url_params, logger)
+  logger.debug "answer count:#{answers['count']}"
+  url_params = nil
+  answers['results'].each do |a|
+    logger.ap a
+    answer_number += 1
+    logger.debug "ANSWER number:#{answer_number}"
+    id = a['id']
+    question = a['question']
+    created = a['created']
+    updated = a['updated']
+    logger.debug "created from API: #{created} <-- this is PST not UTC despite the 'Z'"
+    # All times returned by the API are in PST not PDT and not UTC
+    # All URL parameters for time are also in PST not UTC
+    # See https://github.com/mozilla/kitsune/issues/3961 and
+    # https://github.com/mozilla/kitsune/issues/3946
+    # The above may change in the future if we migrate the Kitsune database to UTC
+
+    created = kludge_time_from_bogusZ_to_utc(created)
+    logger.debug "created with PST correction: #{created}"
+
+    unless updated.nil?
+      logger.debug "updated from API: #{updated} <-- this is PST not UTC despite the 'Z'"
+      updated = kludge_time_from_bogusZ_to_utc(a['updated'])
+      logger.debug "updated with PST correction: #{updated}"
+    end
+    s_question_url = "https://support.mozilla.org/question/#{question}"
+    csv.push([id, question, s_question_url, a['content'], created,
+              a['creator']['username'], a['creator']['display_name'],
+              updated, a['updated_by'], a['is_spam'], a['num_helpful_votes'],
+              a['num_unhelpful_votes']])
+  end
+  url = answers['next']
+  if url.nil?
+    logger.debug 'nil next url'
+    end_program = true
+    next
+  else
+    logger.debug "next url:#{url}"
+    sleep(0.5) # sleep 0.5 seconds between API calls
+  end
+end
+headers = %w[id question question_url content created creator display_name updated\
+             updated_by is_spam num_helpful_votes num_unhelpful_votes]
+CSV.open("#{CREATOR}-answers.csv", 'w', write_headers: true, headers: headers) do |csv_object|
+  csv.each { |row_array| csv_object << row_array }
+end
--- get-tb-creator-answers-questions-for-arbitrary-time-period.rb
+++ get-tb-creator-answers-questions-for-arbitrary-time-period.rb
@@ -9,33 +9,10 @@ require 'date'
 require 'csv'
 require 'logger'
 require_relative 'get-kitsune-response'
+require_relative 'fix-kludged-time'
 
 logger = Logger.new(STDERR)
 logger.level = Logger::DEBUG
-
-def kludge_time_from_bogusZ_to_utc(pst_in_utc_str)
-  pst_in_utc = Time.parse(pst_in_utc_str)
-  old_tz = ENV['TZ']
-  ENV['TZ'] = 'America/Vancouver'
-  t = Time.local(
-    pst_in_utc.year,
-    pst_in_utc.month,
-    pst_in_utc.day,
-    pst_in_utc.hour,
-    pst_in_utc.min,
-    pst_in_utc.sec
-  )
-  # https://github.com/rtanglao/rt-kits-api3/issues/1
-  fixed_time =
-    if t.dst?
-      Time.parse(pst_in_utc_str.gsub('Z', 'PDT'))
-    else
-      Time.parse(pst_in_utc_str.gsub('Z', 'PST'))
-    end
-  ENV['TZ'] = old_tz
-  fixed_time
-end
-
 if ARGV.length < 6
   puts "usage: #{$0} yyyy mm dd end-yyyy mm id"
   exit
