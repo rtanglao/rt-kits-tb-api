2dcffc0540444336200ee9fd93d4cf3135be2b5c
Roland Tanglao <roland@rolandtanglao.com>
2024-04-01 12:18:22 -0700

    first working version

--- remove-non-thunderbird-lines.rb
+++ remove-non-thunderbird-lines.rb
@@ -20,7 +20,17 @@ CSV.foreach(CSV_WITH_GA, headers: true).each do |ga_line|
   ga_l = ga_line.to_hash
   ap ga_l
   next if ga_l['Page'].nil?
+
   page = ga_l['Page'].downcase
-  tb_ga_csv.push(ga_l) if page.include? 'thunderbird' ||slugs.include?(page)
+  tb_ga_csv.push(ga_l) if page.include?('thunderbird') || slugs.include?(page)
 end
 ap tb_ga_csv
+ap tb_ga_csv.first
+ap tb_ga_csv.first.keys 
+
+CSV.open('junk.csv', "w") do |csv|
+  csv << tb_ga_csv.first.keys # adds the attributes name on the first line
+  tb_ga_csv.each do |hash|
+    csv << hash.values
+  end
+end
