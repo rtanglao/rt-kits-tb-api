170f5062f00d9f2c35643e3a93df9050c5032cef
Roland Tanglao <roland@rolandtanglao.com>
2025-05-19 23:10:33 -0700

    works except for 404s

--- get-locale-leaders-reviewers-editors-active-contributors-last90.rb
+++ get-locale-leaders-reviewers-editors-active-contributors-last90.rb
@@ -14,34 +14,46 @@ end
 CSV_LOCALES_FILE = ARGV[0]
 mechanize = Mechanize.new
 output_csv = []
+YYYY_MM_DD = Time.now.strftime('%Y-%m-%d')
 CSV.foreach(CSV_LOCALES_FILE, headers: true).each do |locale_info|
   locale_url = locale_info['locale_url']
   logger.debug "locale_url #{locale_url}"
   page = mechanize.get(locale_url)
-  locale_leaders = ''
-  page.css('.leaders .author-name').each do |l|
-    locale_leaders += "#{l.attr('href')};"
-  end
-    locale_reviewers = ''
-  page.css('.reviewers .author-name').each do |l|
-    locale_reviewers += "#{l.attr('href')};"
-  end
+  locale_leaders = page.css('.leaders a.author-name').map do |link|
+    "https://support.mozilla.org#{link['href']}"
+  end.join(';')
+  locale_reviewers = page.css('.reviewers a.author-name').map do |link|
+    "https://support.mozilla.org#{link['href']}"
+  end.join(';')
+  locale_editors = page.css('.editors a.author-name').map do |link|
+    "https://support.mozilla.org#{link['href']}"
+  end.join(';')
+  active_contributors_last90 = page.css('.active a.author-name').map do |link|
+    "https://support.mozilla.org#{link['href']}"
+  end.join(';')
+  locale_url = locale_info['locale_url']
+  locale_row =
+    {
+      locale_url: locale_url,
+      locale_leaders: locale_leaders,
+      locale_reviewers: locale_reviewers,
+      locale_editors: locale_editors,
+      active_contributors_last90: active_contributors_last90
+    }
+  logger.debug("locale_url: #{locale_url}")
   logger.debug "locale_leaders: #{locale_leaders.ai}"
   logger.debug "locale_reviewers: #{locale_reviewers.ai}"
-
-  exit
-  page.css('html.js body.html-ltr.logged-in.responsive.en-US div#main-content.mzp-l-content div.sumo-page-section--inner div.sumo-l-two-col main.sumo-l-two-col--main article#locale-listing div#locale-leaders.editable.sumo-page-section--inner ul.users.leaders.cf li div.avatar-details.user-meta div.user div.asked-by')
-  page.css('.translated_locale').map { |link| link['href'] }.each do |localized_slug|
-    fromuri = "https://support.mozilla.org#{localized_slug}"
-    logger.debug "fromuri:#{fromuri}"
-    from_uri = URI(fromuri)
-    Net::HTTP.start(from_uri.host, from_uri.port, use_ssl: from_uri.scheme == 'https') do |http|
-      request = Net::HTTP::Get.new from_uri.request_uri
-      response = http.request request # Net::HTTPResponse object
-      response_uri = response['location']
-      localized_slug_after_redirect = response_uri.nil? ? localized_slug : response_uri
-      puts "#{kb_slug.chomp},#{localized_slug_after_redirect}"
-      sleep(1) # Keep Kitsune from throttling this script :-)
-    end
-  end
+  logger.debug "locale_editors: #{locale_editors.ai}"
+  logger.debug "active_contributors_last90: #{active_contributors_last90.ai}"
+  output_csv.push(locale_row)
+  sleep(1) # Keep Kitsune from throttling this script :-)
+end
+headers = output_csv[0].keys
+CSV.open(
+  "#{YYYY_MM_DD}-locale-leaders-reviewers-editors-active-last90.csv",
+  'w', 
+  write_headers: true,
+  headers: headers
+) do |csv_object|
+  csv.each { |row_array| csv_object << row_array }
 end
